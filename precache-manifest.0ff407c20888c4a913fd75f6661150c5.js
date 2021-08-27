cf7f1494b458f80266991d94fa99f5e93a5c2dff
Carlos Diaz <carlosa.diazf@farmatodo.com.co>
2021-08-27 10:01:24 -0500

    Adding built version

---
+++ precache-manifest.0ff407c20888c4a913fd75f6661150c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36fbbbaded7f72ca1fd9",
+    "url": "/static/css/main.0363bf4e.chunk.css"
+  },
+  {
+    "revision": "36fbbbaded7f72ca1fd9",
+    "url": "/static/js/main.36fbbbad.chunk.js"
+  },
+  {
+    "revision": "4c16cd80fda38bbbf629",
+    "url": "/static/js/1.4c16cd80.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42c61aef36a240a045ce91ae1fcf20dc",
+    "url": "/index.html"
+  }
+];
